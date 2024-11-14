298e37a2385d6ccdba1f8114ad937507b0a230aa
adjiri564 <danielsackey564@gmail.com>
2024-11-14 11:08:04 +0000

    anime website

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './index.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import AnimeList from './components/AnimeList';
+import AnimeDetails from './components/AnimeDetails';
+import CurrentAnimes from './components/CurrentAnime';
+
+const App = () =>{
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element ={<AnimeList />}/>
+        <Route path='/anime/:id' element ={<AnimeDetails />}/>
+      </Routes>
+    </Router>
+    // <div className='bg-gray-100 min-h-screen p-4'>
+    //   <h1 className='text-3xl font-bold text-center mb-6'>Welcome to the Anime Website</h1>
+    //   <AnimeList />
+    //   {/* <CurrentAnimes /> */}
+    // </div>
+  )
+}
+
+export default App;
